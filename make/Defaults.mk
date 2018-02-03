e37dc6e45ec8bdf46a704138440f41154ade1d31
Ryan Stone <rstone@FreeBSD.org>
2018-02-03 00:48:49 -0500

    Build with debug symbols

--- make/Defaults.mk
+++ make/Defaults.mk
@@ -10,5 +10,5 @@ C_OPTIM=-O3 -fno-omit-frame-pointer
 CXX_STD=-std=c++17
 CXX_WARNFLAGS=-Wall -Werror -Wno-user-defined-literals
 
-CFLAGS:=-I/usr/local/include -I$(TOPDIR)/include $(C_OPTIM)
+CFLAGS:=-I/usr/local/include -I$(TOPDIR)/include $(C_OPTIM) -g
 CXXFLAGS:=$(CXX_STD) $(CXX_WARNFLAGS)

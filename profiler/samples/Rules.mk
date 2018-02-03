addf2e1bbb76ea180745cbe024c90814bbd0790f
Ryan Stone <rstone@FreeBSD.org>
2018-02-03 00:55:38 -0500

    Add AddressSpace test to new build

--- profiler/samples/Rules.mk
+++ profiler/samples/Rules.mk
@@ -9,3 +9,22 @@ SUBDIRS := \
 	aggfactory \
 	spacefactory \
 
+TESTS := \
+	AddressSpace \
+
+TEST_ADDRESSSPACE_SRCS= \
+	AddressSpace.o \
+
+TEST_ADDRESSSPACE_LIBS := \
+	frame \
+	sharedptr \
+	imagefactory \
+
+TEST_ADDRESSSPACE_WRAPFUNCS= \
+	close=mock_close \
+	exit=mock_exit \
+	fprintf=mock_fprintf \
+	open=mock_open \
+
+TEST_ADDRESSSPACE_STDLIBS= \
+	gmock \

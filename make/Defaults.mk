7cf53447ffb041472773c58a3196ee051a6653e0
Ryan Stone <rstone@FreeBSD.org>
2021-08-19 18:55:59 -0400

    Fix some ISO C++ idiocy
    Some tests couldn't compile because the C++ standard decided to
    break perfectly legitimate compound assignments to C structures.
    Disable the biggest source of idiocy with a compiler flag and
    appease the compiler in a couple of other places.

--- make/Defaults.mk
+++ make/Defaults.mk
@@ -12,7 +12,7 @@ LD=${CXX}
 C_OPTIM=-O3 -fno-omit-frame-pointer
 
 CXX_STD=-std=c++17
-CXX_WARNFLAGS=-Wall -Werror -Wno-user-defined-literals
+CXX_WARNFLAGS=-Wall -Werror -Wno-reorder-init-list
 
 CFLAGS:=-I/usr/local/include -I$(TOPDIR)/include $(C_OPTIM) -g
 CXXFLAGS:=$(CXX_STD) $(CXX_WARNFLAGS)

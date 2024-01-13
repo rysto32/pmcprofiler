606e6d49d6008cd230c76be0c0d6d794a35527be
Ryan Stone <rstone@FreeBSD.org>
2024-01-13 13:55:19 -0500

    Add a test for Profile Printer
    In all honesty this is old stuff I'm just committing to get it
    out of the way.  Tests seem to pass at least.

--- profiler/printers/Rules.mk
+++ profiler/printers/Rules.mk
@@ -5,3 +5,15 @@ SRCS := \
 	CallchainProfilePrinter.cpp \
 	ProfilePrinter.cpp \
 
+TESTS := \
+	ProfilePrinter \
+
+TEST_PROFILEPRINTER_SRCS := \
+	ProfilePrinter \
+
+TEST_PROFILEPRINTER_LIBS := \
+	sharedptr \
+	frame \
+	samples \
+
+

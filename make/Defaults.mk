3ea70b385f7364ef40848c1593cbb54d1919cc23
Ryan Stone <rstone@FreeBSD.org>
2024-01-14 21:33:52 -0500

    Switch to C++20

--- make/Defaults.mk
+++ make/Defaults.mk
@@ -11,8 +11,9 @@ LD=${CXX}
 
 C_OPTIM=-O3 -fno-omit-frame-pointer
 
-CXX_STD=-std=c++17
-CXX_WARNFLAGS=-Wall -Werror -Wno-reorder-init-list
+CXX_STD=-std=c++20
+CXX_WARNFLAGS=-Wall -Werror
+# -Wno-reorder-init-list
 
 CFLAGS:=-I/usr/local/include -I$(TOPDIR)/include $(C_OPTIM) -g
 CXXFLAGS:=$(CXX_STD) $(CXX_WARNFLAGS)
